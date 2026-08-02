66a6a0fdeed62d4457274c7822d42b4eca3e8d1d
Chandana <159031538+madhu-chandu@users.noreply.github.com>
2026-08-02 14:49:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-db_url = os.getenv("DATABASE_URL")
+host = os.getenv("MYSQLHOST")
+port = os.getenv("MYSQLPORT")
+user = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+database = os.getenv("MYSQLDATABASE")
+
+db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
 engine = create_engine(db_url)
 
-session= sessionmaker(
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
